1afee3004496ce75f395cd597dad74aa44099c27
Rhafael Chandra <109317539+rhafaelc@users.noreply.github.com>
2024-06-02 12:09:53 +0700

    feat: connection to flowers database

--- lib/models/flower_model.dart
+++ lib/models/flower_model.dart
@@ -1,32 +1,31 @@
-// flower_model.dart
 class Flower {
-  final int id;
+  final String id;
   final String name;
   final String imageUrl;
-  final double price;
   final String type;
   final String size;
-  final String description;  // Add description field
+  final double price;
+  final String description;
 
   Flower({
     required this.id,
     required this.name,
     required this.imageUrl,
-    required this.price,
     required this.type,
     required this.size,
+    required this.price,
     required this.description,
   });
 
   factory Flower.fromJson(Map<String, dynamic> json) {
     return Flower(
-      id: json['id'],
+      id: json['_id'],
       name: json['name'],
       imageUrl: json['imageUrl'],
-      price: json['price'],
       type: json['type'],
       size: json['size'],
-      description: json['description'],  // Parse description
+      price: json['price'].toDouble(),
+      description: json['description'],
     );
   }
 }
--- lib/services/flower_service.dart
+++ lib/services/flower_service.dart
@@ -1,70 +1,18 @@
-// flower_service.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:dekora/models/flower_model.dart';
 
 class FlowerService {
+  static const String _baseUrl = 'http://localhost:5000/flowers';
+
   static Future<List<Flower>> fetchFlowers() async {
-    return [
-      Flower(
-        id: 1,
-        name: 'Dahlia',
-        imageUrl: 'assets/images/flower1.png',
-        price: 31.4,
-        type: 'Indoor',
-        size: 'Medium',
-        description:
-            'Chrysanthemums, Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,Chrysanthemums,v or simply "mums," are vibrant flowers with diverse colors and shapes. They symbolize happiness and longevity in many cultures and are commonly used in celebrations like weddings and festivals. Mums are easy to grow in sunny spots and well-drained soil, making them perfect for gardens or containers.',
-      ),
-      Flower(
-        id: 2,
-        name: 'Roses',
-        imageUrl: 'assets/images/flower2.png',
-        price: 25.0,
-        type: 'Outdoor',
-        size: 'Small',
-        description:
-            'Roses are a symbol of love and romance. They come in a variety of colors and sizes and are perfect for gardens and bouquets.',
-      ),
-      Flower(
-        id: 3,
-        name: 'Roses',
-        imageUrl: 'assets/images/flower2.png',
-        price: 25.0,
-        type: 'Outdoor',
-        size: 'Small',
-        description:
-            'Roses are a symbol of love and romance. They come in a variety of colors and sizes and are perfect for gardens and bouquets.',
-      ),
-      Flower(
-        id: 4,
-        name: 'Roses',
-        imageUrl: 'assets/images/flower2.png',
-        price: 25.0,
-        type: 'Outdoor',
-        size: 'Small',
-        description:
-            'Roses are a symbol of love and romance. They come in a variety of colors and sizes and are perfect for gardens and bouquets.',
-      ),
-      Flower(
-        id: 5,
-        name: 'Roses',
-        imageUrl: 'assets/images/flower2.png',
-        price: 25.0,
-        type: 'Outdoor',
-        size: 'Small',
-        description:
-            'Roses are a symbol of love and romance. They come in a variety of colors and sizes and are perfect for gardens and bouquets.',
-      ),
-      Flower(
-        id: 6,
-        name: 'Roses',
-        imageUrl: 'assets/images/flower2.png',
-        price: 25.0,
-        type: 'Outdoor',
-        size: 'Small',
-        description:
-            'Roses are a symbol of love and romance. They come in a variety of colors and sizes and are perfect for gardens and bouquets.',
-      ),
-      // Add more flowers with complete details here
-    ];
+    final response = await http.get(Uri.parse(_baseUrl));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((flower) => Flower.fromJson(flower)).toList();
+    } else {
+      throw Exception('Failed to load flowers');
+    }
   }
 }
