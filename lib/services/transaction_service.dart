42b28455a3be50bee6ae736ac45d9fb8a7053731
Arel Sulistyo <nathanaelaurelinosulistyo@mail.ugm.ac.id>
2024-06-13 13:50:57 +0700

    fix: make sure everything is working

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -6,7 +6,7 @@ import 'package:dekora/models/transaction_model.dart';
 class TransactionService {
   static const String _baseUrl = 'http://localhost:5000/transactions';
 
-  static Future<String> createTransaction(
+  static Future<String?> createTransaction(
       Map<String, dynamic> transaction) async {
     final User? user = FirebaseAuth.instance.currentUser;
     if (user == null) {
@@ -23,12 +23,19 @@ class TransactionService {
       },
       body: json.encode(transaction),
     );
-    print(response.statusCode);
-    print(response.body);
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
 
     if (response.statusCode == 201) {
       final responseData = json.decode(response.body);
-      return responseData['snapToken'];
+      final snapToken = responseData['snapToken'];
+      if (snapToken != null && snapToken is String) {
+        return snapToken;
+      } else {
+        // If snapToken is missing, return a default message or handle accordingly
+        return null;
+      }
     } else {
       throw Exception('Failed to create transaction');
     }
